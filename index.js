ec8728789cabcaf07b76a33737b95ccc656493b4
Alex Stone <Stonealex145@gmail.com>
2021-11-07 14:36:58 +0000

    Final Major Commit

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+
+
+
+/////////////////////////////////////////////
+var lightTheme = true;
+
 document.addEventListener("click", ()=>{
-    document.documentElement.style.setProperty('--fg', 'white');
-    document.documentElement.style.setProperty("--bg", "black");
+    if (lightTheme) {
+        document.documentElement.style.setProperty('--fg', 'white');
+        document.documentElement.style.setProperty("--bg", "black");
+    }
+    else {
+        document.documentElement.style.setProperty('--fg', 'black');
+        document.documentElement.style.setProperty("--bg", "white");
+    }
+    lightTheme = !lightTheme;
 })
